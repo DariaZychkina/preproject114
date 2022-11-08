6465f90be9986fcf447e90d006da23007790e00a
DariaZychkina <99396496+DariaZychkina@users.noreply.github.com>
2022-11-08 20:25:01 +0300

    Update UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Connection;
@@ -11,31 +12,134 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
-    }
+    private Connection conn = null;
+
+    public UserDaoJDBCImpl() { }
 
     public void createUsersTable() {
-        inputtingSQLQuery("CREATE TABLE IF NOT EXISTS Users (Id BIGINT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), Lastname VARCHAR(20), Age TINYINT)");
+        PreparedStatement createTab = null;
+        try {
+            conn = Util.getMySQLConnection();
+            conn.setAutoCommit(false);
+            createTab = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Users (Id BIGINT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), Lastname VARCHAR(20), Age TINYINT)");
+            createTab.executeUpdate();
+            conn.commit();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            try {//не понимаю, как тут без вложенных проверок, если методы их требуют
+                if (conn != null ) {
+                    conn.rollback();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        } finally {
+            try {
+                if (createTab != null) {
+                    createTab.close();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        }
     }
 
     public void dropUsersTable() {
-        inputtingSQLQuery("DROP TABLE Users");
+        PreparedStatement dropTable = null;
+        try {
+            conn = Util.getMySQLConnection();
+            conn.setAutoCommit(false);
+            dropTable = conn.prepareStatement("DROP TABLE Users");
+            dropTable.executeUpdate();
+            conn.commit();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            try {       //и тут
+                if (conn != null ) {
+                    conn.rollback();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        } finally {
+            try {
+                if (dropTable != null) {
+                    dropTable.close();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        inputtingSQLQuery("INSERT INTO Users(Name, Lastname, Age) VALUES('" + name +"','"+ lastName + "'," + age + ")");
+        PreparedStatement savingUser = null;
+        try {
+            conn = Util.getMySQLConnection();
+            conn.setAutoCommit(false);
+            savingUser = conn.prepareStatement("INSERT INTO Users(Name, Lastname, Age) VALUES(?,?,?)");
+            savingUser.setString(1, name);
+            savingUser.setString(2, lastName);
+            savingUser.setByte(3, age);
+            savingUser.executeUpdate();
+            conn.commit();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            try { //и вот тут еще
+                if (conn != null ) {
+                    conn.rollback();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        } finally {
+            try {
+                if (savingUser != null) {
+                    savingUser.close();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        }
     }
 
     public void removeUserById(long id) {
-        inputtingSQLQuery("DELETE FROM Users WHERE Id = "+ id);
+        PreparedStatement remUser = null;
+        try {
+            conn = Util.getMySQLConnection();
+            conn.setAutoCommit(false);
+            remUser = conn.prepareStatement("DELETE FROM Users WHERE Id = ?");
+            remUser.setLong(1, id);
+            remUser.executeUpdate();
+            conn.commit();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            try {  //кхм, да-да и тут
+                if (conn != null ) {
+                    conn.rollback();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        } finally {
+            try {
+                if (remUser != null) {
+                    remUser.close();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        }
     }
 
     public List<User> getAllUsers() {
-        String sqlComand = "SELECT * FROM Users";
         List<User> users = new LinkedList<>();
-        try (Connection conn = Util.getMySQLConnection()) {
-            Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery(sqlComand);
+        PreparedStatement getUsers = null;
+        try {
+            conn = Util.getMySQLConnection();
+            conn.setAutoCommit(false);
+            getUsers = conn.prepareStatement("SELECT * FROM Users");
+            ResultSet rs = getUsers.executeQuery();
             while (rs.next()) {
                 User user = new User();
                 user.setId(rs.getLong("Id"));
@@ -44,26 +148,52 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(rs.getByte("Age"));
                 users.add(user);
             }
-            return users;
+            conn.commit();
         } catch (SQLException | ClassNotFoundException e) {
             try {
-                throw  new SQLException();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
+                if (conn != null ) {
+                    conn.rollback();
+                }
+            } catch (SQLException excep) {
+                throw new SQLException();
             }
+        } finally { //везде в общем + нет возможности использовать try с ресурсами, тк нужен rollback, который происходит при исключении, но при try с ресурсами коннекта, конечно, видно не будет
+            try {
+                if (getUsers != null) {
+                    getUsers.close();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+            return users;
         }
     }
 
     public void cleanUsersTable() {
-        inputtingSQLQuery("DELETE FROM Users");
-    }
-
-    public void inputtingSQLQuery(String sqlQuery){
-        try (Connection conn = Util.getMySQLConnection()) {
-            Statement statement = conn.createStatement();
-            statement.executeUpdate(sqlQuery);
+        PreparedStatement cleanTab = null;
+        try {
+            conn = Util.getMySQLConnection();
+            conn.setAutoCommit(false);
+            cleanTab = conn.prepareStatement("DELETE FROM Users");
+            cleanTab.executeUpdate();
+            conn.commit();
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
+            try {
+                if (conn != null ) {
+                    conn.rollback();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
+        } finally {
+            try {
+                if (cleanTab != null) {
+                    cleanTab.close();
+                }
+            } catch (SQLException excep) {
+                System.out.println(excep.getMessage());
+            }
         }
     }
 }
